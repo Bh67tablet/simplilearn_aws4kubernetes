1eeafcd64041e4dc0c09a8f3ec788a92ccefb36f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-23 10:56:37 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@
 		vpc = "vpc-0566166c204b7eca4"
 		ami = "ami-0fc5d935ebf8bc3bc"
 		itype = "t2.micro"
-		subnet = "subnet-0c4d6ac22631a5e56"
+		subnet = "subnet-06e127888fec6f91a"
 		publicip = true
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg"  
