30d2ff4fc92dceadbf69b8a2cfa9ff8a1e778c90
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-24 07:40:53 +0200

    Update variables.tf

--- awsWorker/variables.tf
+++ awsWorker/variables.tf
@@ -5,10 +5,10 @@
  variable "ec2_parameters" { 
    default = { 
 		region = "us-east-1"
-		vpc = "vpc-0566166c204b7eca4"
+		vpc = "vpc-0b7844fe1d4df504f"
 		ami = "ami-0fc5d935ebf8bc3bc"
 		itype = "t2.micro"
-		subnet = "subnet-06e127888fec6f91a"
+		subnet = "subnet-0040d5134f06f1d98"
 		publicip = true
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg"  
