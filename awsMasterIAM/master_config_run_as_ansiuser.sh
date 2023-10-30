f6d986a5560a159e5f9281916b452b6f2fb5d9ac
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-30 15:15:12 +0100

    Update master_config_run_as_ansiuser.sh

--- awsMasterIAM/master_config_run_as_ansiuser.sh
+++ awsMasterIAM/master_config_run_as_ansiuser.sh
@@ -3,5 +3,6 @@ mkdir /home/ansiuser/.ssh
 chmod 700 /home/ansiuser/.ssh
 ssh-keygen -q -t rsa -f /home/ansiuser/.ssh/id_rsa -N '' <<< $'\ny' >/dev/null 2>&1
 echo [webserver] > /home/ansiuser/myinventory
+sudo chown ansisuer:ansiuser /home/ansiuser/ips
 for ip in $(cat /home/ansiuser/ips); do echo $ip >> /home/ansiuser/myinventory; done
 cat /home/ansiuser/myinventory
