a9b2d147b19c9a31ae3533350b6700d54a5deb55
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-31 07:49:47 +0100

    Update master_ssh_copy_id.sh

--- awsMasterIAM/master_ssh_copy_id.sh
+++ awsMasterIAM/master_ssh_copy_id.sh
@@ -1,4 +1,7 @@
-sudo chown ansisuer:ansiuser /home/ansiuser/ips
+#!/usr/bin/env bash
+mkdir /home/ansiuser/.ssh
+chmod 700 /home/ansiuser/.ssh
+ssh-keygen -q -t rsa -f /home/ansiuser/.ssh/id_rsa -N '' <<< $'\ny' >/dev/null 2>&1
 #for ip in $(cat /home/ansiuser/ips); do sshpass -p ansiuser ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip; done
 for ip in $(cat /home/ansiuser/ips); do echo "ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip"; done
 for ip in $(cat /home/ansiuser/ips); do echo "ssh -i $HOME/.ssh/id_rsa.pub ansiuser@$ip"; done
