42fc0be84ab72a1cea5c40ba6bc8d29e622c3e88
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-31 07:24:03 +0100

    Update variables.tf

--- awsMasterIAM/variables.tf
+++ awsMasterIAM/variables.tf
@@ -5,14 +5,14 @@
  variable "ec2_parameters" { 
    default = { 
 		region = "us-east-1"
-		vpc = "vpc-02072287009b3f29c"
+		vpc = "vpc-0e830c64ce04494b0"
 		ami = "ami-0fc5d935ebf8bc3bc"
 		itype = "t2.micro"
-		subnet = "subnet-010708c3ba9901af5"
+		subnet = "subnet-0ee287dfb7f3142f7"
 		publicip = true
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg" 
 		iam_instance_profile   = "test_profile"
-		vpc_security_group_ids = ["sg-0a133eafe001284df"]
+		vpc_security_group_ids = ["sg-0164dd95dd71ac5c4"]
 	} 
  }
