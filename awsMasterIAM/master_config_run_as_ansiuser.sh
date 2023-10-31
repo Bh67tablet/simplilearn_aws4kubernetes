8a2706acd7ae09ba52b7a422e3cae0ac37eaba8d
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-10-31 07:42:43 +0100

    Update master_config_run_as_ansiuser.sh

--- awsMasterIAM/master_config_run_as_ansiuser.sh
+++ awsMasterIAM/master_config_run_as_ansiuser.sh
@@ -3,6 +3,6 @@ mkdir /home/ansiuser/.ssh
 chmod 700 /home/ansiuser/.ssh
 ssh-keygen -q -t rsa -f /home/ansiuser/.ssh/id_rsa -N '' <<< $'\ny' >/dev/null 2>&1
 echo [webserver] > /home/ansiuser/myinventory
-sudo chown ansisuer:ansiuser /home/ansiuser/ips
+echo $(grep -w "private_ip".*, terraform.tfstate | cut -d"\"" -f4) >> ips
 for ip in $(cat /home/ansiuser/ips); do echo $ip >> /home/ansiuser/myinventory; done
 cat /home/ansiuser/myinventory
