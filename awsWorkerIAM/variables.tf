b9205f8f41321a30ca1e9bde1d58b716cec17de2
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-02 06:27:02 +0100

    Update variables.tf

--- awsWorkerIAM/variables.tf
+++ awsWorkerIAM/variables.tf
@@ -1,14 +1,18 @@
  variable "region" { 
    default ="us-east-1" 
  } 
+
+variable "vpc_id" {
+  type        = string
+}
   
  variable "ec2_parameters" { 
    default = { 
 		region = "us-east-1"
-		vpc = "vpc-0e830c64ce04494b0"
+		vpc = var.vpc_id
 		ami = "ami-0fc5d935ebf8bc3bc"
 		itype = "t2.micro"
-		subnet = "subnet-0ee287dfb7f3142f7"
+		subnet = "subnet-0783c8ac905ea38ba"
 		publicip = true
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg"  
