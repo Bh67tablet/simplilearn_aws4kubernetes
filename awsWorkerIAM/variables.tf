daa4971e0656b0765ddaed2aa3f47b98bf1f516f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-02 06:32:23 +0100

    Update variables.tf

--- awsWorkerIAM/variables.tf
+++ awsWorkerIAM/variables.tf
@@ -9,7 +9,6 @@ variable "vpc_id" {
  variable "ec2_parameters" { 
    default = { 
 		region = "us-east-1"
-		vpc = var.vpc_id
 		ami = "ami-0fc5d935ebf8bc3bc"
 		itype = "t2.micro"
 		subnet = "subnet-0783c8ac905ea38ba"
