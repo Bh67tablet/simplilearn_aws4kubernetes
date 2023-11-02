827bd7b3f201fc213f00939e477e813c967be641
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-02 06:47:59 +0100

    Update main.tf

--- awsWorkerIAM/main.tf
+++ awsWorkerIAM/main.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "bh67sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
