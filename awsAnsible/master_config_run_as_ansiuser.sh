48e57e6577f7696ed522a3a72ab47cb148c4d1c3
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-03 10:14:54 +0100

    Add files via upload

---
+++ awsAnsible/master_config_run_as_ansiuser.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo [webserver] > /home/ansiuser/myinventory
+echo $(grep -w "private_ip".*, /home/ansiuser/terraform.tfstate | cut -d"\"" -f4) >> /home/ansiuser/ips
+for ip in $(cat /home/ansiuser/ips); do echo $ip >> /home/ansiuser/myinventory; done
+cat /home/ansiuser/myinventory
+mkdir /home/ansiuser/.ssh
+chmod 700 /home/ansiuser/.ssh
+ssh-keygen -q -t rsa -f /home/ansiuser/.ssh/id_rsa -N '' <<< $'\ny' >/dev/null 2>&1
+#for ip in $(cat /home/ansiuser/ips); do sshpass -p ansiuser ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip; done
+for ip in $(cat /home/ansiuser/ips); do echo "ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip"; done
+for ip in $(cat /home/ansiuser/ips); do echo "ssh -i $HOME/.ssh/id_rsa ansiuser@$ip"; done
+echo "run above commands"
+echo "for ip in $(cat /home/ansiuser/ips); do ssh ansiuser@$ip ls -la $HOME/.ssh; done"
+echo "for ip in $(cat /home/ansiuser/ips); do ssh ansiuser@$ip cat $HOME/.ssh/authorized_keys; done"
