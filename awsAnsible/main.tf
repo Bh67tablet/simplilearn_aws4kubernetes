04f4282881fb1b150c49a1a31417770347f429e7
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-03 15:23:50 +0100

    Update main.tf

--- awsAnsible/main.tf
+++ awsAnsible/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "awsAnsible" {
 	associate_public_ip_address 	= var.ec2_parameters.publicip
 	key_name 			= var.ec2_parameters.keyname
 	iam_instance_profile   		= var.ec2_parameters.iam_instance_profile
-	vpc_security_group_ids 		= ["sg-0865b7e3581e25dfc"]
+	vpc_security_group_ids 		= ["sg-0e2bcfcbd5ed2a965"]
 	tags = {
 	    # The count.index allows you to launch a resource 
 	    # starting with the distinct index number 0 and corresponding to this instance.
