1285db497ab7370ff22cb59bc8c750ad6cf0f35f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-03 13:52:43 +0100

    Update main.tf

--- awsAnsible/main.tf
+++ awsAnsible/main.tf
@@ -18,27 +18,9 @@ resource "aws_instance" "awsAnsible" {
   	}
 user_data = <<EOF
 #! /bin/bash
-echo "I was here">/var/tmp/greetings.txt
-sudo apt -y update >>/var/tmp/yum.update 2>&1
-sudo apt -y install git >>/var/tmp/yum.update 2>&1
-sudo apt-get install -y software-properties-common >>/var/tmp/yum.update 2>&1
-sudo apt-add-repository ppa:ansible/ansible >>/var/tmp/yum.update 2>&1
-sudo apt-get update >>/var/tmp/yum.update 2>&1
-sudo apt-get install -y ansible >>/var/tmp/yum.update 2>&1
-sudo useradd -m ansiuser -s /bin/bash -p 'ansiuser' >>/var/tmp/yum.update 2>&1
-sudo echo "ansiuser:ansiuser" | chpasswd >>/var/tmp/yum.update 2>&1
-sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.bak >>/var/tmp/yum.update 2>&1
-sudo cp -p /etc/sudoers /etc/sudoers.bak >>/var/tmp/yum.update 2>&1
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config >>/var/tmp/yum.update 2>&1
-sudo sed -i "s/^root.*$/root    ALL=(ALL:ALL) ALL\nansiuser ALL=NOPASSWD: ALL/g" /etc/sudoers >>/var/tmp/yum.update 2>&1
-sudo systemctl restart sshd >>/var/tmp/yum.update 2>&1
-sudo diff /etc/ssh/sshd_config /etc/ssh/sshd_config.bak >>/var/tmp/yum.update 2>&1
-sudo diff /etc/sudoers /etc/sudoers.bak >>/var/tmp/yum.update 2>&1
-sudo apt install -y awscli >>/var/tmp/yum.update 2>&1
-sudo ansible-galaxy collection install amazon.aws -y >>/var/tmp/yum.update 2>&1
-sudo apt install python3-pip -y >>/var/tmp/yum.update 2>&1
-sudo su - -c 'su - ansiuser -c "aws s3 cp s3://bh67-githubactions-bucket/terraform.tfstate ."'
-sudo su - -c 'su - ansiuser -c "whoami"' >>/var/tmp/yum.update 2>&1
+sudo sh /home/ansiuser/simplilearn_aws/master_config_run_as_root.sh >>/var/tmp/yum.update 2>&1
 sudo su - -c 'su - ansiuser -c "git clone https://github.com/Bh67tablet/simplilearn_aws.git"' >>/var/tmp/yum.update 2>&1
+sudo chmod 755 /home/ansiuser/simplilearn_aws/*.sh >>/var/tmp/yum.update 2>&1
+sudo su - -c 'su - ansiuser -c /home/ansiuser/simplilearn_aws/master_config_run_as_ansiuser.sh' >>/var/tmp/yum.update 2>&1
 EOF
 }
