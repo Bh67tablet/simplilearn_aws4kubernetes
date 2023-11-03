54ecc566e0a976a1599ac7c4542a97524d016b7f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-03 14:02:36 +0100

    Update master_config_run_as_ansiuser.sh

--- awsAnsible/master_config_run_as_ansiuser.sh
+++ awsAnsible/master_config_run_as_ansiuser.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
-echo [webserver] > /home/ansiuser/myinventory
+#!/bin/bash
+aws s3 cp s3://bh67-githubactions-bucket/terraform.tfstate .
 echo $(grep -w "private_ip".*, /home/ansiuser/terraform.tfstate | cut -d"\"" -f4) >> /home/ansiuser/ips
+echo [webserver] > /home/ansiuser/myinventory
 for ip in $(cat /home/ansiuser/ips); do echo $ip >> /home/ansiuser/myinventory; done
-cat /home/ansiuser/myinventory
+#
 mkdir /home/ansiuser/.ssh
 chmod 700 /home/ansiuser/.ssh
+for ip in $(cat /home/ansiuser/ips); do sshpass -pansiuser ssh -o StrictHostKeyChecking=no ansiuser@$ip rm -rf .ssh; done
 ssh-keygen -q -t rsa -f /home/ansiuser/.ssh/id_rsa -N '' <<< $'\ny' >/dev/null 2>&1
-#for ip in $(cat /home/ansiuser/ips); do sshpass -p ansiuser ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip; done
-for ip in $(cat /home/ansiuser/ips); do echo "ssh-copy-id -i $HOME/.ssh/id_rsa.pub ansiuser@$ip"; done
-for ip in $(cat /home/ansiuser/ips); do echo "ssh -i $HOME/.ssh/id_rsa ansiuser@$ip"; done
-echo "run above commands"
-echo "for ip in $(cat /home/ansiuser/ips); do ssh ansiuser@$ip ls -la $HOME/.ssh; done"
-echo "for ip in $(cat /home/ansiuser/ips); do ssh ansiuser@$ip cat $HOME/.ssh/authorized_keys; done"
+for ip in $(cat /home/ansiuser/ips); do sshpass -pansiuser ssh-copy-id -i /home/ansiuser/.ssh/id_rsa.pub ansiuser@$ip; done
+for ip in $(cat /home/ansiuser/ips); do ssh -o StrictHostKeyChecking=no ansiuser@$ip ls -la; done
+for ip in $(cat /home/ansiuser/ips); do ssh -o StrictHostKeyChecking=no ansiuser@$ip ls -la .ssh; done
+for ip in $(cat /home/ansiuser/ips); do ssh -o StrictHostKeyChecking=no ansiuser@$ip uname -a; done
