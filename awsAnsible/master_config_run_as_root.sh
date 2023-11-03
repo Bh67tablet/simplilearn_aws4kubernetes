aa567523c28832368c648b2dcf9e42238d9454f1
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-03 17:09:12 +0100

    Update master_config_run_as_root.sh

--- awsAnsible/master_config_run_as_root.sh
+++ awsAnsible/master_config_run_as_root.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 whoami
-echo "I was here"
 apt -y update
 apt -y install git
 apt-get install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt-get update
+apt-add-repository -y ppa:ansible/ansible
+apt -y update
 apt-get install -y ansible
-diff /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-diff /etc/sudoers /etc/sudoers.bak
 apt install -y awscli
 ansible-galaxy collection install amazon.aws -y
 apt install python3-pip -y
