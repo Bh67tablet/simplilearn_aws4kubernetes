eeb2f30816f2028eef8be5ee91cc1e1819e68b0d
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-10 21:50:11 +0100

    Update ec2.tf

--- StateExample1c/ec2.tf
+++ StateExample1c/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                    = "ami-03e08697c325f02ab"
+  ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.micro"
   key_name               = "bh67"
   subnet_ids = [data.terraform_remote_state.global.outputs.lambda_subnet_id]
