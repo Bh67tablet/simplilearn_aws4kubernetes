bc1773e27806e10ff5dd670a8f2033a5bc00f236
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-10 06:16:35 +0100

    Update backend.tf

--- awsAnsible/backend.tf
+++ awsAnsible/backend.tf
@@ -7,7 +7,28 @@ terraform {
 }
 
 #### refering the vpc (resource-1) remote backend
+
 data "terraform_remote_state" "first_subnet_id" {
+  backend = "s3"
+
+  config = {
+    bucket = "bh67-githubactions-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "aws_security_group_name" {
+  backend = "s3"
+
+  config = {
+    bucket = "bh67-githubactions-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "vpc_id" {
   backend = "s3"
 
   config = {
