1a6907b73e92b05f5fbf45e924e058338f1b6945
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-10 22:09:23 +0100

    Delete StateExample1b directory

--- StateExample1b/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "global" {
-  backend = "s3"
-  config = {
-    bucket = "bh67-githubactions-bucket"
-    key	   = "global/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
