6a9de5188aaa8bc784c28adc26d24d232924adf2
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-10 14:01:58 +0100

    Delete 0modules directory

--- 0modules/module1/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bh67"
-    key    = "dev/vpc/main/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- 0modules/module1/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-  value = aws_vpc.vpc[*].id
-}
-
-output "aws_security_group_name" {
-  value = aws_security_group.sec-group-vpc-ssh-icmp[*].id
-}
