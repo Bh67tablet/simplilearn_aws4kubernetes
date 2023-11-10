4e29e3b00942a2f87daef20a43a40c6077848761
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-10 07:14:39 +0100

    Update variables.tf

--- awsAnsible/variables.tf
+++ awsAnsible/variables.tf
@@ -11,6 +11,6 @@ variable "ec2_parameters" {
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg" 
 		iam_instance_profile   = "test_profile"
-		#subnet_id ="subnet-00eeb860859108603"
+		subnet_id ="subnet-0c11c8af59aaa8e88"
 	} 
 }
