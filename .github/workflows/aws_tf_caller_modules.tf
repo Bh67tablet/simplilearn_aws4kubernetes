52b06e70e6552a030de3ef585b71cbd7315162c6
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-12 08:53:06 +0100

    Update aws_tf_caller_modules.tf

--- .github/workflows/aws_tf_caller_modules.tf
+++ .github/workflows/aws_tf_caller_modules.tf
@@ -22,5 +22,3 @@ jobs:
         AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
         AWS_SECRET_KEY: ${{ secrets.AWS_SECRET_KEY }}
         AWS_SESSION_TOKEN: ${{ secrets.AWS_SESSION_TOKEN }}
-
- 
