3cead9873c1b0e7bdab696b375d721d985c62f7a
Bh67tablet <bh67tablet@gmail.com>
2023-11-12 20:00:43 +0100

    copy Ansible dirs

---
+++ AnsibleMaster/variables.tf
@@ -0,0 +1,16 @@
+variable "region" { 
+default ="us-east-1" 
+} 
+
+variable "ec2_parameters" { 
+	default = { 
+		region = "us-east-1"
+		ami = "ami-0fc5d935ebf8bc3bc"
+		itype = "t2.micro"
+		publicip = true
+		keyname = "simplilearn_key"
+		secgroupname = "bh67sg" 
+		iam_instance_profile   = "test_profile"
+		subnet_id ="subnet-0808b3939180673fa"
+	} 
+}
