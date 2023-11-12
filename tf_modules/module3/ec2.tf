16463779b28c620b9a13db09f64c69ab3db8e5e0
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-12 15:28:57 +0100

    Update ec2.tf

--- tf_modules/module3/ec2.tf
+++ tf_modules/module3/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2master" {
-	count = 2
+	count = 1
 	ami                    = "ami-0fc5d935ebf8bc3bc"
 	instance_type          = "t2.micro"
 	key_name               = "simplilearn_key"
