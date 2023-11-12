1197d0827c9ec182df13ef0eb422e48e55185375
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-12 09:39:34 +0100

    Update data.tf

--- tf_modules/module2/data.tf
+++ tf_modules/module2/data.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "global" {
   backend = "s3"
   config = {
-    bucket = "bh67-githubactions-bucket"
+    bucket = "bh67"
     key	   = "global/terraform.tfstate"
     region = "us-east-1"
   }
