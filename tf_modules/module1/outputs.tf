7416eb20d9d04abdfd6b1963c73d16dfca12e846
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-12 09:25:32 +0100

    Update outputs.tf

--- tf_modules/module1/outputs.tf
+++ tf_modules/module1/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
   value   	= aws_vpc.main.id
+  description = "ID of the main VPC"
 }
  
-output "subnet_id" {
-  value   	= aws_subnet.subnet_id.id
+output "lambda_subnet_id" {
+  value   	= aws_subnet.lambda.id
+  description = "ID of the subnet dedicated to Lambdas"
 }
  
-output "security_group_id" {
-  value   	= aws_security_group.sg.id
+output "tls_security_group_id" {
+  value   	= aws_security_group.allow_tls.id
+  description = "ID of the allow_tls security group"
 }
