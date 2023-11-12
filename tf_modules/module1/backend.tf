eb3e19a7fe170bb24f121ae5610b8b6b192b1c74
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-12 08:17:18 +0100

    Update backend.tf

--- tf_modules/module1/backend.tf
+++ tf_modules/module1/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "bh67_S3_Bucket"
-    key = "global/terraform.tfstate"
+    key = "tf_modules/module1/terraform.tfstate"
     region = "us-east-1"
   }
 }
