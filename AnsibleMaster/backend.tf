7c0c7a525a6b1c9fe90be267a4ac50137299427d
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-13 07:52:40 +0100

    Update backend.tf

--- AnsibleMaster/backend.tf
+++ AnsibleMaster/backend.tf
@@ -1,3 +1,12 @@
+data "terraform_remote_state" "AnsibleWorker" {
+  backend = "s3"
+  config = {
+    bucket = "bh67-githubactions-bucket"
+    key	   = "AnsibleWorker/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "bh67-githubactions-bucket"
