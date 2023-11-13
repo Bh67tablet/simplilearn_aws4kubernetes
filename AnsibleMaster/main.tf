e35f8dcc30af82b53421929f06115d2f3b7fec8f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-13 07:54:49 +0100

    Update main.tf

--- AnsibleMaster/main.tf
+++ AnsibleMaster/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "AnsibleMaster" {
 	associate_public_ip_address 	= var.ec2_parameters.publicip
 	key_name 			= var.ec2_parameters.keyname
 	iam_instance_profile   		= var.ec2_parameters.iam_instance_profile
-	vpc_security_group_ids 		= ["sg-0d9a973426764dc7e"]
+	vpc_security_group_ids 		= [data.terraform_remote_state.AnsibleWorker.outputs.bh67_sec_group]
 	tags = {
 	    # The count.index allows you to launch a resource 
 	    # starting with the distinct index number 0 and corresponding to this instance.
