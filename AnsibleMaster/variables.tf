3f2e4b0168efc95590b4909b07db420db75a0e78
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-22 08:09:00 +0100

    Update variables.tf

--- AnsibleMaster/variables.tf
+++ AnsibleMaster/variables.tf
@@ -10,7 +10,7 @@ variable "ec2_parameters" {
 	default = { 
 		region = "us-east-1"
 		ami = "ami-0fc5d935ebf8bc3bc"
-		itype = "t2.medium"
+		itype = "t2.micro"
 		publicip = true
 		keyname = "simplilearn_key"
 		secgroupname = "bh67sg" 
