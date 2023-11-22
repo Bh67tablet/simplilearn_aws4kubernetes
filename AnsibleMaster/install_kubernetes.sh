4b1e393861430d6d9d45b99f87a4acccd4cbe868
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-22 06:25:08 +0100

    Update install_kubernetes.sh

--- AnsibleMaster/install_kubernetes.sh
+++ AnsibleMaster/install_kubernetes.sh
@@ -1,17 +1,17 @@
 sudo usermod -aG sudo ansiuser
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get -y update
+sudo apt-get -y install apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+sudo apt-get -y update
+sudo apt-get -y install kubelet kubeadm kubectl docker
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ansiuser
 sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
 sudo systemctl restart docker.service
