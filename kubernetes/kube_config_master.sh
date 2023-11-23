e8c5285510bd48906af21f1ae62ae5388a4bbfcb
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-23 06:49:03 +0100

    Add files via upload

---
+++ kubernetes/kube_config_master.sh
@@ -0,0 +1,11 @@
+kubeadm config images pull
+echo ‘{"exec-opts": ["native.cgroupdriver=systemd"]}’ | tee /etc/docker/daemon.json
+systemctl daemon-reload
+systemctl restart docker
+systemctl restart kubelet
+kubeadm init
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
+kubectl get nodes
