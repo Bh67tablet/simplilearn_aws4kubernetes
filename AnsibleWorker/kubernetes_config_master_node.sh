efd04752deb3b8da937de33b59e3cae420e2584b
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-11-24 06:17:20 +0100

    Create kubernetes_config_master_node.sh

---
+++ AnsibleWorker/kubernetes_config_master_node.sh
@@ -0,0 +1,7 @@
+sudo kubeadm init
+mkdir -p $HOME/.kube 
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
+sudo kubeadm token create --print-join-command > kube_token.txt
+aws s3 cp kube_token.txt s3://bh67-githubactions-bucket/
