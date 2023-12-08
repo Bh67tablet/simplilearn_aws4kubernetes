850cebcde40ac5e978ffdaf2a39d4b1f6227b645
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-08 14:11:53 +0100

    Create securtiy_group_mysql_bastion.tf

---
+++ terraform/securtiy_group_mysql_bastion.tf
@@ -0,0 +1,31 @@
+# Creating security group for MySQL Bastion Host Access
+resource "aws_security_group" "DB-SG-SSH" {
+
+  depends_on = [
+    aws_vpc.custom,
+    aws_subnet.subnet1,
+    aws_subnet.subnet2,
+    aws_security_group.BH-SG
+  ]
+
+  description = "MySQL Bastion host access for updates!"
+  name = "mysql-sg-bastion-host"
+  vpc_id = aws_vpc.custom.id
+
+  # Created an inbound rule for MySQL Bastion Host
+  ingress {
+    description = "Bastion Host SG"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.BH-SG.id]
+  }
+
+  egress {
+    description = "output from MySQL BH"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
