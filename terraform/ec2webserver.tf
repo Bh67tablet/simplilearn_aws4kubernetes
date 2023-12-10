e5e8b8c0f04f912424f83050d627d0a2055b0b0b
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-10 10:02:51 +0100

    Update ec2webserver.tf

--- terraform/ec2webserver.tf
+++ terraform/ec2webserver.tf
@@ -24,19 +24,4 @@ resource "aws_instance" "webserver" {
   tags = {
    Name = "Webserver_From_Terraform"
   }
-
-  # Code for installing the softwares!
-  provisioner "remote-exec" {
-    inline = [
-        "sudo yum update -y",
-        "sudo yum install php php-mysqlnd httpd -y",
-        "wget https://wordpress.org/wordpress-4.8.14.tar.gz",
-        "tar -xzf wordpress-4.8.14.tar.gz",
-        "sudo cp -r wordpress /var/www/html/",
-        "sudo chown -R apache.apache /var/www/html/",
-        "sudo systemctl start httpd",
-        "sudo systemctl enable httpd",
-        "sudo systemctl restart httpd"
-    ]
-  }
 }
