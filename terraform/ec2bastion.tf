974a656b19b90593cacbb0afcae14d114a01bc40
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-10 09:58:18 +0100

    Update ec2bastion.tf

--- terraform/ec2bastion.tf
+++ terraform/ec2bastion.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Bastion-Host" {
      aws_instance.MySQL
   ]
   
-  ami = "ami-0162dd7febeafb455"
+  ami = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet1.id
 
