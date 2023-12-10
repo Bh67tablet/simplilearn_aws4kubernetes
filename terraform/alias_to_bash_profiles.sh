09cb3ecabfecba6e0b9dcb2865dcd93f9c3b77b0
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-10 11:09:56 +0100

    Update alias_to_bash_profiles.sh

--- terraform/alias_to_bash_profiles.sh
+++ terraform/alias_to_bash_profiles.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+cat > /home/ec2-user/.bash_profiles << EOF
 alias ti='terraform init'
 alias tp='terraform plan'
 alias ta='terraform apply --auto-approve'
 alias td='terraform destroy  --auto-approve'
+EOF
