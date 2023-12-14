2f0147fd481db6093accb2b9cbf54e7c4d2c7a27
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-14 14:02:04 +0100

    Update install_jenkins.sh

--- AnsibleMaster/install_jenkins.sh
+++ AnsibleMaster/install_jenkins.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 sudo apt install default-jre -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
