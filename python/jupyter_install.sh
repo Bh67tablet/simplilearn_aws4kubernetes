31d83153a0096c486c7ab23c285eed60bbe2d496
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-29 22:27:39 +0100

    Update jupyter_install.sh

--- python/jupyter_install.sh
+++ python/jupyter_install.sh
@@ -4,9 +4,14 @@
 #
 # https://dataschool.com/data-modeling-101/running-jupyter-notebook-on-an-ec2-server/
 #
-sudo apt-get install bzip2
+#sudo apt-get install bzip2
+#
+sudo yum -y install expect
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-sudo bash Anaconda3-2019.03-Linux-x86_64.sh
+#
+bash Anaconda3-2019.03-Linux-x86_64.sh
+#
+source ~/.bashrc
 jupyter notebook --generate-config
 cat > $HOME/.jupyter/jupyter_notebook_config.py << EOF
 conf = get_config()
@@ -14,10 +19,12 @@ conf.NotebookApp.ip = '0.0.0.0'
 conf.NotebookApp.password = u'YOUR PASSWORD HASH'
 conf.NotebookApp.port = 8888
 EOF
-# https://github.com/Mohdwajtech/Complete-Python-3-Bootcamp.git
-git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
+#
 # set your password
 #jupyter notebook password
 #
 # start:
 jupyter notebook
+#
+# https://github.com/Mohdwajtech/Complete-Python-3-Bootcamp.git
+git clone https://github.com/Bh67tablet/Complete-Python-3-Bootcamp.git
