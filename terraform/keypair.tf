5eef47625a8ac56675dffcd469639ab09d012a2a
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-02-13 09:00:46 +0100

    Update keypair.tf

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -2,7 +2,7 @@
 resource "aws_key_pair" "Key-Pair" {
 
   # Name of the Key
-  key_name   = "MyKeyFinal"
+  key_name   = "simplilearn_key"
 
   # Adding the SSH authorized key !
   public_key = file("~/.ssh/authorized_keys")
